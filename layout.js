97dd44d639da309ade292e72575ede3963a0ca6b
shopthetop247-web <shopthetop247@gmail.com>
2025-12-07 19:45:01 -0500

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "shopthetop247",
+  description: "Learn • Shop • Save"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
