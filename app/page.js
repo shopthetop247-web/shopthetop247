8e4b2a0576daea7a88a9785b9c672539a4094dcc
shopthetop247-web <shopthetop247@gmail.com>
2025-12-07 20:10:11 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>shopthetop247</h1>
+      <p>Welcome to your Next.js site.</p>
+    </main>
+  );
+}
