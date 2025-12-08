de5e8d1b8deb07e1cae780cca11d7f75a4832a2d
shopthetop247-web <shopthetop247@gmail.com>
2025-12-07 19:55:52 -0500

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
-      <h1>shopthetop247</h1>
-      <p>Welcome to the demo Next.js site.</p>
-    </main>
-  );
-}
