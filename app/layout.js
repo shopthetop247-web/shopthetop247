e35d752a7d643f61292d8b386b8a168eb33229d3
shopthetop247-web <shopthetop247@gmail.com>
2025-12-22 09:16:53 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100 text-gray-900">
+      <body className="bg-gray-100 text-brand-secondary">
         <Header />
         {children}
         <Footer />
