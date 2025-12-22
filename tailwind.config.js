8c9316f90a4f427f531011e62da209ded3c933f4
shopthetop247-web <shopthetop247@gmail.com>
2025-12-22 09:49:17 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         brand: {
           primary: "#1F2937",   // Main brand Text Dark Slate
           secondary: "#1F2937", // Dark slate (headers, footer)
-          accent: "#A6D98F",    // Light Green (links, highlights)
+          accent: "#65A653",    // Darker Green (links, highlights)
           muted: "#6B7280",     // Muted text
           background: "#F2D8A7" // Light Yellow
         },
