56c40ee4e9e3b1c9d7e02c9aa3c59a6135b6977e
shopthetop247-web <shopthetop247@gmail.com>
2025-12-23 09:03:23 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -62,7 +62,7 @@ export default function Home() {
             {/* FEATURED BLOG CARD */}
             <a 
               href={`/blog/${latestPost.slug}`}
-              className="block bg-color: #d9d0bf p-5 rounded-2xl shadow-sm hover:shadow-md transition"
+              className="block bg-color: #f5ae2a p-5 rounded-2xl shadow-sm hover:shadow-md transition"
             >
               <div className="flex gap-4 items-start">
 
